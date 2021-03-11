a1c71fa252321759e422b0840c8851ceef1ffbc4
pngdhty <pangestuadhitya11@gmail.com>
2021-03-11 13:04:54 +0700

    validasi edit profile

--- app/Http/Livewire/Account/Edit.php
+++ app/Http/Livewire/Account/Edit.php
@@ -12,33 +12,73 @@ class Edit extends Component
     use WithFileUploads;
 
     public $name, $username, $picture;
-
+    
+    /**
+     * mount
+     *
+     * @return void
+     */
     public function mount()
     {
         $this->name = auth()->user()->name;
         $this->username = auth()->user()->username;
     }
-
-    public function edit()
+    
+    /**
+     * rules validation
+     *
+     * @return void
+     */
+    public function rules()
     {
-       if ($this->picture) {
-
-           Storage::delete(auth()->user()->picture);
-           $picture = $this->picture->store('images/profile');
-
-       } else {
-           $picture = auth()->user()->picture ?? null;
-       }
-
-       auth()->user()->update([
-           'name' => $this->name,
-           'username' => $this->username,
-           'picture' => $picture
-       ]);
-
-       return redirect(route('setting.edit'));
+        return [
+            'name' => 'required|min:3|string',
+            'username' => 'required|min:3|max:25|string|unique:users,username,' . auth()->id(),
+            'picture' => $this->picture ? 'image|mimes:jpg,jpeg,png|max:1024' : '',
+        ];
     }
 
+    public function updated($fieldName)
+    {
+        $this->validateOnly($fieldName,[
+            'name' => 'min:3|string',
+            'username' => 'required|min:3|max:25|string|unique:users,username,' . auth()->id(),
+            'picture' => 'image|mimes:jpg,jpeg,png|max:1024'
+        ]);
+    }
+    
+    /**
+     * edit profile
+     *
+     * @return void
+     */
+    public function edit()
+    {
+        $this->validate();
+
+        if ($this->picture) {
+
+            Storage::delete(auth()->user()->picture);
+            $picture = $this->picture->store('images/profile');
+
+        } else {
+            $picture = auth()->user()->picture ?? null;
+        }
+
+        auth()->user()->update([
+            'name' => $this->name,
+            'username' => $this->username,
+            'picture' => $picture
+        ]);
+
+         return redirect(route('setting.edit'));
+    }
+    
+    /**
+     * render
+     *
+     * @return void
+     */
     public function render()
     {
         return view('livewire.account.edit');
--- resources/views/livewire/account/edit.blade.php
+++ resources/views/livewire/account/edit.blade.php
@@ -5,15 +5,24 @@
         <form wire:submit.prevent="edit">
             <div class="mb-2">
                 <label for="name" class="block mb-1 font-medium">Name</label>
-                <input id="name" type="text" class="w-full border-gray-300 rounded" wire:model="name">
+                <input id="name" type="text" class="w-full border-gray-300 rounded @error('name') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" wire:model="name">
+                @error('name')
+                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-5">
                 <label for="picture" class="block mb-1 font-medium capitalize">profile picture</label>
-                <input id="picture" type="file" class="w-full border-gray-300 rounded" wire:model="picture">
+                <input id="picture" type="file" class="w-full border-gray-300 rounded @error('picture') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" wire:model="picture">
+                @error('picture')
+                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-2">
                 <label for="username" class="block mb-1 font-medium">Username</label>
-                <input id="username" type="text" class="w-full border-gray-300 rounded" wire:model="username">
+                <input id="username" type="text" class="w-full border-gray-300 rounded @error('username') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" wire:model="username">
+                @error('username')
+                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mt-5">
                 <button type="submit" class="flex justify-center px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700">
